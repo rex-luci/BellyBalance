cf275487ea521e4fe6a973152c5f21cd0d9376d9
rex-luci <83593249+rex-luci@users.noreply.github.com>
2024-06-05 10:36:37 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,79 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+     
+      animation: {
+        fadeInSlideUp: 'fadeInSlideUp 1s ease-out',
+
+        'slide-down': 'slide-down 1s ease-out'
+      },
+      keyframes: {
+        
+        fadeInSlideUp: {
+          '0%': { transform: 'translateY(10%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        'slide-down': {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1'  },
+        }
+      },
+        fontSize: {
+          '72': '72px'
+        },
+        rotate: {
+          '10': '10deg',
+        },
+      colors: {
+        
+          'CAF4FF': '#CAF4FF',
+        
+        green: {
+          50: '#30AF5B',
+          90: '#292C27',
+        },
+        gray: {
+          10: '#EEEEEE',
+          20: '#A2A2A2',
+          30: '#7B7B7B',
+          50: '#585858',
+          90: '#141414',
+        },
+        orange: {
+          50: '#FF814C',
+        },
+        blue: {
+          70: '#021639',
+        },
+        yellow: {
+          50: '#FEC601',
+        },
+      },
+      backgroundImage: {
+        'bg-img-1': "url('/img-1.png')",
+        'bg-img-2': "url('/img-2.png')",
+        'feature-bg': "url('/feature-bg.png')",
+        pattern: "url('/pattern.png')",
+        'pattern-2': "url('/pattern-bg.png')",
+      },
+      screens: {
+        xs: '400px',
+        '3xl': '1680px',
+        '4xl': '2200px',
+      },
+      maxWidth: {
+        '10xl': '1512px',
+      },
+      borderRadius: {
+        '5xl': '40px',
+      },
+    },
+  },
+  plugins: [],
+};
